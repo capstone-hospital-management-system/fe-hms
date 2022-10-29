f79b3e2fa09d8c69e0f91fe2207f77a2654d5fdd
KangAbbad <naufalabbad9@gmail.com>
2022-10-29 23:37:12 +0700

    fix: initial testing
    [UNITTEST] Medicine - CRUD #37

--- src/app/medicines/components/medicines.component.spec.ts
+++ src/app/medicines/components/medicines.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { MedicinesComponent } from './medicines.component';
 
 describe('MedicinesComponent', () => {
   let component: MedicinesComponent;
   let fixture: ComponentFixture<MedicinesComponent>;
+  const fakeActivatedRoute = {
+    queryParams: of({ page: 1, per_page: 5 }),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MedicinesComponent],
+      imports: [MedicinesComponent, HttpClientTestingModule],
+      providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MedicinesComponent);
--- src/app/treatments/components/treatments.component.spec.ts
+++ src/app/treatments/components/treatments.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { TreatmentsComponent } from './treatments.component';
 
 describe('TreatmentsComponent', () => {
   let component: TreatmentsComponent;
   let fixture: ComponentFixture<TreatmentsComponent>;
+  const fakeActivatedRoute = {
+    queryParams: of({ page: 1, per_page: 5 }),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TreatmentsComponent],
+      imports: [TreatmentsComponent, HttpClientTestingModule],
+      providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TreatmentsComponent);
